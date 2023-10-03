c49e096540cca9edc5f4631c860daf7502f39f5d
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-10-03 17:40:25 -0500

    init dbcontext and model

---
+++ ASPNETCoreReactAppointmentManager/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ASPNETCoreReactAppointmentManager.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETCoreReactAppointmentManager.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Appointment> Appointments { get; set; }
+    }
+}
---
+++ ASPNETCoreReactAppointmentManager/Data/Models/Appointment.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASPNETCoreReactAppointmentManager.Data.Models
+{
+    public class Appointment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(150), Column(TypeName = "nvarchar(150)")]
+        public string Title { get; set; } = "Title";
+
+        [MaxLength(300), Column(TypeName = "nvarchar(300)")]
+        public string Description { get; set; } = "Description";
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime ModifiedDate { get; set; } = DateTime.Now;
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        [MaxLength(300), Column(TypeName = "nvarchar(300)")]
+        public string Address { get; set; } = "Address";
+
+        [MaxLength(10), Column(TypeName = "nvarchar(10)")]
+        public string Time { get; set; } = "12:30";
+        public bool Done { get; set; } = false;
+        public bool Deleted { get; set; } = false;
+        public byte LevelOfImportance { get; set; } = 2;
+
+
+
+
+    }
+}
